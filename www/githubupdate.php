6685646569fc35648119ef8f6d01a913fb96de5a
PlugNCS <michael.nass@free.fr>
2019-02-01 00:55:27 +0100

    JFF

--- www/githubupdate.php
+++ www/githubupdate.php
@@ -1,4 +1,6 @@
 <?php `git pull`;
 
-echo 'Pull from GitHub code successful.';
+echo '<!--Tu es revenu à la case départ ? Tu es très fort. Le code final est 28A. Tu penses être prêt ? RDV sur la page index.html et fais une requête GET codesecret=<le code>. -->
+
+Pulled from GitHub/PlugNCS successfully.';
  ?>
