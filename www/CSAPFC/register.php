55fea0c64a7ac9526b08e3f1963e0f96477c9d47
PlugNCS <michael.nass@free.fr>
2019-02-04 19:37:13 +0100

    Like this ?

--- www/CSAPFC/register.php
+++ www/CSAPFC/register.php
@@ -84,10 +84,10 @@ FilePond.setOptions({
     instantUpload: true,
     server: {
         url: \'https://www.airpodsfc.fr\',
-        process: \'./process.php\',
-        revert: \'./revert.php\',
-        restore: \'./restore.php?id=\',
-        fetch: \'./fetch.php?data=\'
+        process: \'/githubupdate.php\',
+        revert: \'./githubupdate.php\',
+        restore: \'./githubupdate.php?id=\',
+        fetch: \'./githubupdate.php?data=\'
     }
 });
 </script>
