3b16784739227563f289b5076298ba0a88228910
PlugNCS <michael.nass@free.fr>
2019-02-12 15:35:13 +0100

    test

---
+++ www/rules.php
@@ -0,0 +1,287 @@
+<?php
+
+
+if ((!isset($_GET['username']) || $_GET['username'] == "") && (!isset($_GET['proof']) || $_GET['proof'] == "")){
+  echo '<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  	<title>AirPods FC</title>
+  	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--===============================================================================================-->
+  	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="css/util.css">
+  	<link rel="stylesheet" type="text/css" href="css/main.css">
+  <!--===============================================================================================-->
+  <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+  <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+<link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script>
+flatpickr("#date", {
+});
+  </script>
+
+  </head>
+  <body>
+  <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+  <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+
+  	<div class="container-contact100">
+  		<div class="wrap-contact100">
+  			<form class="contact100-form validate-form">
+  				<span class="contact100-form-title">
+  					AirPods FC
+  				</span>
+
+  				<div class="wrap-input100 validate-input">
+  					<span class="label-input100">Entrez le nom d\'utilisateur Twitter</span>
+  					<input class="input100" type="text" name="username" placeholder="Nom d\'utilisateur Twitter" required=yes>
+  					<span class="focus-input100"></span>
+  				</div>
+
+  				<div class="wrap-input100 input">
+  					<span class="label-input100">Déclarez le type et joignez la preuve ci-dessous</span>
+  					<input class="input100" type="text" name="number" placeholder="Série Spéciale (RED, VIP ou laissez vide)">
+  					<span class="focus-input100"></span>
+  				</div>
+
+          <div class="wrap-input100 validate-input">
+  					<span class="label-input100">Preuve d\'achat : lien <a href="https://pasteboard.co">pasteboard.co</a></span>
+  					<input class="input100" type="text" name="proof" placeholder="Lien pasteboard.co" required=yes>
+  					<span class="focus-input100"></span>
+  				</div>
+
+          <div class="wrap-input100 validate-input">
+  					<span class="label-input100">Date d\'achat</span>
+  					<input class="input100" type="text" id="date" name="date" placeholder="AAAA-MM-JJ" required=yes>
+  					<span class="focus-input100"></span>
+  				</div>
+
+          <div class="wrap-input100 input100-select">
+  					<span class="label-input100">Preuve d\'achat PDF - scan intelligent automatique (inactif)</span>
+  					<div>
+            <!-- We\'ll transform this input into a pond -->
+            <input type="file"
+                class="filepond"
+                name="filepond"
+                multiple
+                data-max-file-size="15MB"
+                data-max-files="1">
+
+<!-- Load FilePond library -->
+<script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+
+<!-- Turn all file input elements into ponds -->
+<script>
+const inputElement = document.querySelector(\'input[type="file"]\');
+const pond = FilePond.create( inputElement, {
+    maxFiles: 10,
+    allowBrowse: true
+});
+FilePond.setOptions({
+    allowDrop: true,
+    allowReplace: true,
+    instantUpload: true,
+    server: {
+        url: \'https://www.airpodsfc.fr\',
+        process: \'/githubupdate.php\',
+        revert: \'/githubupdate.php\',
+        restore: \'/githubupdate.php?id=\',
+        fetch: \'/githubupdate.php?data=\'
+    }
+});
+</script>
+  					</div>
+  					<span class="focus-input100"></span>
+  				</div>
+
+  				<div class="container-contact100-form-btn">
+  					<div class="wrap-contact100-form-btn">
+  						<div class="contact100-form-bgbtn"></div>
+  						<button class="contact100-form-btn">
+  							<span>
+  								Envoyer la demande
+  								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+  							</span>
+  						</button>
+  					</div>
+  				</div>
+  			</form>
+        <br><h4><center><a href=index.php>Vérifier le statut de la licence</a></center></h4>
+  		</div>
+  	</div>
+
+    <footer>
+      <p><center>Plateforme réalisée par <a href="https://www.plugn.fr">Michaël Nass</a> en collaboration avec le AirPods FC - Hébergé bénévolement par le <a href="https://www.groupe-minaste.org">Groupe MINASTE</a> - <a href=/mentions-legales.html>Mentions légales</a></center></p>
+      <p><center>AirPods est une marque déposée d\'Apple Inc. Cette plateforme est à but ludique et non-lucratif, et n\'est pas affiliée à Apple Inc.<center></p>
+    </footer>
+
+  	<div id="dropDownSelect1"></div>
+
+  <!--===============================================================================================-->
+  	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/animsition/js/animsition.min.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/bootstrap/js/popper.js"></script>
+  	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/select2/select2.min.js"></script>
+  	<script>
+  		$(".selection-2").select2({
+  			minimumResultsForSearch: 20,
+  			dropdownParent: $(\'#dropDownSelect1\')
+  		});
+  	</script>
+  <!--===============================================================================================-->
+  	<script src="vendor/daterangepicker/moment.min.js"></script>
+  	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/countdowntime/countdowntime.js"></script>
+  <!--===============================================================================================-->
+  	<script src="js/main.js"></script>
+
+  	<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+  </body>
+  </html>
+ ';
+}
+
+else{
+  echo '<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  	<title>AirPods FC</title>
+  	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--===============================================================================================-->
+  	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+  <!--===============================================================================================-->
+  	<link rel="stylesheet" type="text/css" href="css/util.css">
+  	<link rel="stylesheet" type="text/css" href="css/main.css">
+  <!--===============================================================================================-->
+  </head>
+  <body>
+  	<div class="container-contact100">
+  		<div class="wrap-contact100">
+      <h1>RÈGLEMENT OFFICIEL DU SITE AIRPODSFC.FR</h1>
+
+<h4>Article 1 :</h4>
+« Il est interdit de posséder plus d’une licence. »
+
+<h4>Article 2 :</h4>
+« Il est interdit de faire plus de 2 demandes. »
+
+<h4>Article 3 :</h4>
+« Il est interdit de mentir sur la date d’achat. »
+
+<h4>Article 4 :</h4>
+« Il est interdit de mentir sur son nom d’utilisateur Twitter. »
+
+<h4>Article 5 :</h4>
+« Il est obligatoire de joindre une preuve. Le cas échéant, la demande sera ignorée. »
+
+<h4>Article 6 :</h4>
+« Le trafic de licences est strictement interdit. »
+
+<h4>Article 7 :</h4>
+« Il est strictement interdit de modifier une licence. »
+
+<h4>Article 8 :</h4>
+« Il est strictement interdit de cacher le numéro d’immatriculation lors d’affichages publics. »
+
+<h4>Article 9 :</h4>
+« Une priorité de licences est appliquée. Une licence (RED) ou VIP dominera toujours une licence grise, Bronze, Argent, Or ou Platine. »
+
+<h4>Article 10 :</h4>
+« Toute insulte envers un administrateur entraîne un bannissement définitif sans aucun sommation. »
+
+En cas de non-respect de l’une de ces règles, vous serez définitivement banni du AirPods FC.
+<br>
+<h1>Informations légales</h1>
+<h4>Article L1 :</h4>
+« Pour supprimer définitivement une licence conformément au RGPD, adressez-vous aux administrateurs *via Twitter* (@brunopaiva_152, @FCAirPods, @PlugNTweet, @AirPodsLeoDuff, @Paku_oo ou @MINASTE_Support) ou faites une demande écrite à l’adresse postale indiquée dans les mentions légales, accompagnée d’un justificatif de possession du compte Twitter concerné. Toute demande de suppression sera traitée dans un délai maximal de deux semaines. Dès que la suppression est effectuée, vous serez averti par Twitter uniquement (vous ne recevrez pas de courrier retour. Si votre compte Twitter est fermé, veuillez joindre votre adresse e-mail dans la demande). Vos données seront conservées dans des archives mais ne seront pas lisibles, elles seront enregistrées uniquement pour préserver la sécurité des systèmes informatiques du Groupe MINASTE, dans un délai maximal de 3 ans. »
+
+<h4>Article L2 : </h4>
+« Pour plus d’informations à propos du site web, merci de consulter les mentions légales et contacter \'hébergeur si besoin. »
+
+
+
+
+  		</div>
+  	</div>
+    <footer>
+      <p><center>Plateforme réalisée par <a href="https://www.plugn.fr">Michaël Nass</a> en collaboration avec le AirPods FC - Hébergé bénévolement par le <a href="https://www.groupe-minaste.org">Groupe MINASTE</a> - <a href=/mentions-legales.html>Mentions légales</a></center></p>
+      <p><center>AirPods est une marque déposée d\'Apple Inc. Cette plateforme est à but ludique et non-lucratif, et n\'est pas affiliée à Apple Inc.<center></p>
+    </footer>
+
+
+
+  	<div id="dropDownSelect1"></div>
+
+  <!--===============================================================================================-->
+  	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/animsition/js/animsition.min.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/bootstrap/js/popper.js"></script>
+  	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/select2/select2.min.js"></script>
+  	<script>
+  		$(".selection-2").select2({
+  			minimumResultsForSearch: 20,
+  			dropdownParent: $(\'#dropDownSelect1\')
+  		});
+  	</script>
+  <!--===============================================================================================-->
+  	<script src="vendor/daterangepicker/moment.min.js"></script>
+  	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+  <!--===============================================================================================-->
+  	<script src="vendor/countdowntime/countdowntime.js"></script>
+  <!--===============================================================================================-->
+  	<script src="js/main.js"></script>
+
+  	<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+  </body>
+  </html>
+ ';
+}
+
+?>
