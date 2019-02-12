0e592bb3a0aa853e169e1c0c486642fa0d4e4537
PlugNCS <michael.nass@free.fr>
2019-02-12 16:02:02 +0100

    OK

--- www/rules.php
+++ www/rules.php
@@ -34,12 +34,13 @@
 <br>
 <h4>Article 0 :</h4>
 <p>« Pour demander votre licence, vous devez remplir le formulaire de <a href="https://www.airpodsfc.fr/register.php">cette page</a>. Une fois votre demande envoyée, vous devez attendre que votre demande soit validée. Vous pouvez vérifier l\'état de la signature sur la page d\'acceuil. N\'envoyez pas plusieurs demandes consécutives pour le même compte avant un délai de 72h. Lorsque votre licence signée sera disponible, vous pourrez la télécharger en HD depuis la page d\'acceuil. »</p>
-
+  <h4>Article 0 bis :</h4>
+  <p>« Les administrateurs utilisent une technologie nommée FASTSIGN pour signer plus rapidement vos licences et diminuer votre temps d\'attente. Merci de bien remplir correctement le formulaire d\'enregistrement pour ne pas ralentir le processus. »</p>
 <h4>Article 1 :</h4>
 <p>« Il est interdit de posséder plus d’une licence. »</p>
 
 <h4>Article 2 :</h4>
-<p>« Il est interdit de faire plus de 2 demandes. »</p>
+<p>« Il est interdit de faire plus de 2 demandes pour le même compte. »</p>
 
 <h4>Article 3 :</h4>
 <p>« Il est interdit de mentir sur la date d’achat. »</p>
