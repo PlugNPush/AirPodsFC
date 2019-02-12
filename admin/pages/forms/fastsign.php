0848e48bd124a79c03fa0b19bd533a97ac37e17a
PlugNCS <michael.nass@free.fr>
2019-02-12 23:04:15 +0100

    STAGE

--- admin/pages/forms/fastsign.php
+++ admin/pages/forms/fastsign.php
@@ -330,7 +330,7 @@ echo '<!DOCTYPE html>
             echo '[LOG] DELETED FROM IMPORT DB.<br>';
             echo '<h2 class="card-title"><br><br>La licence a bien été signée pour @' . $_GET['username2C'] . '!</h2><br><h3 class="card-description">LICENCE NUMÉRO ' . $number . '<br><a href="https://www.airpodsfc.fr/sign.php?id=' . $_GET['username2C'] . '"><img src="https://www.airpodsfc.fr/sign.php?id=' . $_GET['username2C'] . '" height="20%" width="30%" style="border-radius: 7px; overflow:hidden;"></a>';
           } else {
-            echo '<h2 class="card-title"><br><br>La licence n\'a pas pu être signée pour' . $_GET['username2C'] . '.</h2><br><h3 class="card-description">L\'utilisateur a probablement été banni par le système automatique AirPods FC.<br>';
+            echo '<h2 class="card-title"><br><br>La licence n\'a pas pu être signée pour @' . $_GET['username2C'] . '.</h2><br><h3 class="card-description">L\'utilisateur a probablement été banni par le système automatique AirPods FC.<br>';
           }
 
 
